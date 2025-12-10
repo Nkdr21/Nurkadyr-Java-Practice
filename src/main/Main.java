9723fd0c5b022b70ebe4a9f0542ff757011e5aa5
Nkdr21 <takprosta327@gmail.com>
2025-12-10 08:34:29 +0500

    added test logic

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,8 +3,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Java!");
         String name = "Nurkadyr";
-        int age = 18;
-        System.out.println("My name is" + name);
+        int age = 20;
+        System.out.println("My name is " + name);
         System.out.println("Age " + age);
     }
 }
